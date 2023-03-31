30df9d345313e51f59bc56e3d385ae9e464869e4
ShubhamTambeLDP <129063385+ShubhamTambeLDP@users.noreply.github.com>
2023-03-31 17:53:28 +0530

    Add files via upload

---
+++ Assignment1.java
@@ -0,0 +1,27 @@
+public class Assignment1 {
+    public static void main(String[] args) throws Exception {
+        int x=0 ;
+
+
+
+for (int i=1; i<=100; i++) {
+if (i%3==0 && i%5==0 )  {
+System.out.println("FizzBuzz");
+x=i;
+}
+
+else if ((!(x==i)) && i%5==0) {
+System.out.println("Buzz");
+
+}
+else if (i%3==0 && (!(x==i)) ) {
+System.out.println("Fizz");
+}
+else {
+System.out.println(i);
+}
+
+
+}}}
+    
+
