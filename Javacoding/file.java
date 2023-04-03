5bfbe14079df69e17b8b5b9bd647e3b11b7b8d13
Tambe Shubham <tambe.shubham@zemosolabs.com>
2023-04-03 18:39:04 +0530

    all java assignments

---
+++ Javacoding/file.java
@@ -0,0 +1,17 @@
+
+public class file {
+
+    public static String getName;
+
+    public file(String string) {
+    }
+
+    public boolean createNewFile() {
+        return false;
+    }
+
+    public static String[] list() {
+        return null;
+    }
+
+}
