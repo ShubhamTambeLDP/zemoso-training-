f3d65e75326e164877085852b0506026bb977787
ShubhamTambeLDP <129063385+ShubhamTambeLDP@users.noreply.github.com>
2023-04-05 15:03:52 +0530

    Delete Saving.java

--- Javacoding/src/solidprinciples-singleresponsibility/Saving.java
+++
@@ -1,8 +0,0 @@
-public class Saving {
-
-    public void gold(){
-        System.out.println(" child gets gold from parents"); 
-    
-} public void money(){
-    System.out.println(" child gets money from parents"); 
-}}
