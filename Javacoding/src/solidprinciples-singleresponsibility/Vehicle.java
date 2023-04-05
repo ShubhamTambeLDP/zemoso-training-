70993ba9d3869acb6b265a5c1fcf41ac6c330766
ShubhamTambeLDP <129063385+ShubhamTambeLDP@users.noreply.github.com>
2023-04-05 15:02:35 +0530

    Delete Vehicle.java

--- Javacoding/src/solidprinciples-singleresponsibility/Vehicle.java
+++
@@ -1,9 +0,0 @@
-public class Vehicle {
-    public void car(){
-        System.out.println(" child gets car from parents"); 
-    
-}public void scooty(){
-    System.out.println(" child gets scooty from parents");
-}
-}
-
